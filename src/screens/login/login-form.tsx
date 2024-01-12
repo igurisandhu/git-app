e5ed9312d486d01d1e4c41a264fb825ee968d8c7
“gurjeet.singh1” <gurjeet.singh1@jungleworks.com>
2024-01-12 08:54:49 +0530

    first commit

---
+++ src/screens/login/login-form.tsx
@@ -0,0 +1,76 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button, ControlledInput, Image, Text, View } from '@/ui';
+
+const schema = z.object({
+  name: z.string().optional(),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email('Invalid email format'),
+  password: z
+    .string({
+      required_error: 'Password is required',
+    })
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+const style = {
+  animationImage: {
+    width: 350,
+    height: 350,
+    alignContent: 'center',
+    alignSelf: 'center',
+  },
+};
+
+export type FormType = z.infer<typeof schema>;
+
+export type LoginFormProps = {
+  onSubmit?: SubmitHandler<FormType>;
+};
+
+export const LoginForm = ({ onSubmit = () => {} }: LoginFormProps) => {
+  const { handleSubmit, control } = useForm<FormType>({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <View className="flex-1 justify-center p-4">
+      <Text testID="form-title" variant="h1" className="pb-6 text-center">
+        Welcome
+      </Text>
+
+      <Image
+        style={style.animationImage}
+        source={require('../../../assets/images/animation.gif')}
+      />
+
+      <ControlledInput
+        testID="email-input"
+        control={control}
+        name="email"
+        label="Email"
+      />
+      <ControlledInput
+        testID="password-input"
+        control={control}
+        name="password"
+        label="Password"
+        placeholder="********"
+        secureTextEntry={true}
+      />
+      <Button
+        testID="login-button"
+        label="Login"
+        onPress={handleSubmit(onSubmit)}
+        variant="primary"
+      />
+    </View>
+  );
+};
